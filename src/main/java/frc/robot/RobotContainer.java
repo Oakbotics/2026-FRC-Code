811e7c28021b1924cea89c8b78cb02452a15fe10
MilindMaheshwari <mahemili297@gotvdsb.ca>
2026-01-29 16:29:39 -0500

    Auto is accuratly working and printing odemetry into SmartDashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.shooter.ShooterCommmand;
+import frc.robot.commands.TestAuto;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.shooter.ShooterSubsystem;
@@ -81,21 +82,6 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        // Simple drive forward auton
-        final var idle = new SwerveRequest.Idle();
-        return Commands.sequence(
-            // Reset our field centric heading to match the robot
-            // facing away from our alliance station wall (0 deg).
-            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
-            // Then slowly drive forward (away from us) for 5 seconds.
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(0.5)
-                    .withVelocityY(0)
-                    .withRotationalRate(0)
-            )
-            .withTimeout(5.0),
-            // Finally idle for the rest of auton
-            drivetrain.applyRequest(() -> idle)
-        );
+        return new TestAuto(drivetrain);
     }
 }
---
+++ src/main/java/frc/robot/commands/TestAuto.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class TestAuto extends SequentialCommandGroup {
+
+    public TestAuto(CommandSwerveDrivetrain drivetrain) {
+        
+        Pose2d startingPose = new Pose2d(8.0, 1.0, new Rotation2d(0));
+
+        Pose2d targetPose = new Pose2d(
+            startingPose.getX() + 1.0, 
+            startingPose.getY(), 
+            startingPose.getRotation()
+        );
+
+        addCommands(
+            drivetrain.resetPoseCommand(startingPose),
+
+            drivetrain.findPathToPose(targetPose)
+        );
+    }
+}
