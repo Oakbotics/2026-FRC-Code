b161a52e495588ad7e7218eb88b8e218f040f57b
Alhadi Hamada Ghenniwa <didigh.2010@gmail.com>
2026-03-18 18:38:50 -0400

    chanegd magnet offset

--- src/main/java/frc/robot/wrist/WristConstants.java
+++ src/main/java/frc/robot/wrist/WristConstants.java
@@ -27,9 +27,9 @@ public class WristConstants {
     public static final double statorCurrentLimit = 30.0; // TODO: set actual wrist stator limit
     */
     
-    final double magnetOffset = 0.14;
+    final double magnetOffset = 0.25;
     final SensorDirectionValue sensorDirectionValue = SensorDirectionValue.CounterClockwise_Positive;
     public final int wristMotorId = 7;
     public final static int wristEncoderId = 37;
-    public static final double positionToleranceRotations = 0.25;
+    public static final double positionToleranceRotations = 0.01;
   }
