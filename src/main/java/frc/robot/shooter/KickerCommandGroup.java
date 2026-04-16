0e23feca69953b965123de270987c38c12156181
TughluqDev <tlqanimation@gmail.com>
2026-04-16 16:33:59 -0400

    Implemented middle delayed auto

--- src/main/java/frc/robot/shooter/KickerCommandGroup.java
+++ src/main/java/frc/robot/shooter/KickerCommandGroup.java
@@ -1,48 +1,5 @@
 package frc.robot.shooter;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-// import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-// import edu.wpi.first.wpilibj2.command.RepeatCommand;
-// import edu.wpi.first.wpilibj2.command.RunCommand;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import edu.wpi.first.wpilibj2.command.WaitCommand;
-// import frc.robot.hopper.HopperCommand;
-// import frc.robot.hopper.HopperConstants;
-// import frc.robot.hopper.HopperFeedShootCommand;
-// import frc.robot.hopper.HopperSubsystem;
-// import frc.robot.intake.IntakeAutoStartCommandGroup;
-// import frc.robot.intake.IntakeCommand;
-// import frc.robot.intake.IntakeSubsystem;
-// import frc.robot.roller.RollerSubsystem;
-
-// public class KickerCommandGroup extends SequentialCommandGroup {
-//     public KickerCommandGroup(KickerSubsystem m_kickerSubsystem, RollerSubsystem m_rollerSubsystem, IntakeSubsystem m_intakesubsystem, HopperSubsystem m_hopperSubsystem) {
-//         addCommands(
-//             new ParallelCommandGroup(
-//                 new KickerRollerCommand(m_kickerSubsystem, m_rollerSubsystem),
-//                 new SequentialCommandGroup(
-//                     new WaitCommand(2.0),
-//                     new RepeatCommand(
-//                         new SequentialCommandGroup(
-//                             new HopperCommand(m_hopperSubsystem, 0.20),
-//                             new WaitCommand(0.25),
-//                             new HopperCommand(m_hopperSubsystem, 0.05),
-//                             new WaitCommand(0.25)
-//                         )
-//                     )
-//                 ),
-//                 new IntakeCommand(m_intakesubsystem, 10)
-//             )
-
-//         );
-        
-//     }
-// }
-
-
-// package frc.robot.shooter;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
